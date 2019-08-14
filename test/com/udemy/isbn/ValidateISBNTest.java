8f14ba344c768eb64a011225289efd335505b4c7
Leonardo Narciso <LNarcisoOV@gmail.com>
2019-08-14 13:51:55 -0300

    Adding test for exceptions and isbn with 13 digits.

--- test/com/udemy/isbn/ValidateISBNTest.java
+++ test/com/udemy/isbn/ValidateISBNTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class ValidateISBNTest {
 
 	@Test
-	public void checkIsbnNumber() {
+	public void check10DigitISBNNumber() {
 		ValidateISBN validator = new ValidateISBN();
 		boolean result = validator.checkISBN("0140449116");
 		assertTrue("CheckIsbnNumber - first value.", result);
@@ -16,10 +16,45 @@ public class ValidateISBNTest {
 	}
 	
 	@Test
-	public void checkInvalidIsbnNumber() {
+	public void checkAValid13DigitISBNNumber(){
+		ValidateISBN validator = new ValidateISBN();
+		boolean result = validator.checkISBN("9781853260087");
+		assertTrue(result);
+		result = validator.checkISBN("9781853267338");
+		assertTrue(result);
+	}
+	
+	@Test
+	public void check10DigitISBNNumberEndingInAnXAreValid() {
+		ValidateISBN validator = new ValidateISBN();
+		boolean result = validator.checkISBN("012000030X");
+		assertTrue(result);
+	}	 	
+
+	@Test
+	public void check10DigitInvalidIsbnNumber() {
 		ValidateISBN validator = new ValidateISBN();
 		boolean result = validator.checkISBN("0140449117");
 		assertFalse(result);
+	}
+	
+	@Test
+	public void check13DigitInvalidIsbnNumber() {
+		ValidateISBN validator = new ValidateISBN();
+		boolean result = validator.checkISBN("9781853267336");
+		assertFalse(result);
+	}
+
+	@Test(expected = NumberFormatException.class)
+	public void check9DigitsISBNsAreNotAllowed() {
+		ValidateISBN validator = new ValidateISBN();
+		validator.checkISBN("123456789");
+	}
+	
+	@Test(expected = NumberFormatException.class)
+	public void nonNumeric10DigitISBNsAreNotAllowed() {
+		ValidateISBN validator = new ValidateISBN();
+		validator.checkISBN("helloworld");
 	}
 
 }
