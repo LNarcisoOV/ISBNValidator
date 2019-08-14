5a7d4bee6cb99e9dfef3a7b7bb5f97ab20ea735e
Leonardo Narciso <LNarcisoOV@gmail.com>
2019-08-14 09:52:25 -0300

    Writing first code for isbn validation.

--- src/com/udemy/isbn/ValidateISBN.java
+++ src/com/udemy/isbn/ValidateISBN.java
@@ -2,9 +2,17 @@ package com.udemy.isbn;
 
 public class ValidateISBN {
 
-	public boolean checkISBN(Integer isbn) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+	public boolean checkISBN(String isbn) {
+		int total = 0;
 
+		for (int i = 0; i < 10; i++) {
+			total += isbn.charAt(i) * (10 - i);
+		}
+
+		if (total % 11 == 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
--- test/com/udemy/isbn/ValidateISBNTest.java
+++ test/com/udemy/isbn/ValidateISBNTest.java
@@ -9,14 +9,16 @@ public class ValidateISBNTest {
 	@Test
 	public void checkIsbnNumber() {
 		ValidateISBN validator = new ValidateISBN();
-		boolean result = validator.checkISBN(140449116);
-		assertTrue(result);
+		boolean result = validator.checkISBN("0140449116");
+		assertTrue("CheckIsbnNumber - first value.", result);
+		result = validator.checkISBN("0140177396");
+		assertTrue("CheckIsbnNumber - second value.", result);
 	}
 	
 	@Test
 	public void checkInvalidIsbnNumber() {
 		ValidateISBN validator = new ValidateISBN();
-		boolean result = validator.checkISBN(140449117);
+		boolean result = validator.checkISBN("0140449117");
 		assertFalse(result);
 	}
 
