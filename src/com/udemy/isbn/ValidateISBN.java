646d9518007bcd76f872e5859260e204c5b5b379
Leonardo Narciso <lnarcisoov@gmail.com>
2019-08-13 22:44:44 -0300

    Adding isbn check method and more tests.

--- src/com/udemy/isbn/ValidateISBN.java
+++ src/com/udemy/isbn/ValidateISBN.java
@@ -2,4 +2,9 @@ package com.udemy.isbn;
 
 public class ValidateISBN {
 
+	public boolean checkISBN(Integer isbn) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
 }
--- test/com/udemy/isbn/ValidateISBNTest.java
+++ test/com/udemy/isbn/ValidateISBNTest.java
@@ -7,8 +7,17 @@ import org.junit.Test;
 public class ValidateISBNTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void checkIsbnNumber() {
+		ValidateISBN validator = new ValidateISBN();
+		boolean result = validator.checkISBN(140449116);
+		assertTrue(result);
+	}
+	
+	@Test
+	public void checkInvalidIsbnNumber() {
+		ValidateISBN validator = new ValidateISBN();
+		boolean result = validator.checkISBN(140449117);
+		assertFalse(result);
 	}
 
 }
